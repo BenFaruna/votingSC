fb1eba0a5537042e4ad8e8d4813c4bfc765c8d9c
Benjamin Faruna <benjaminfaruna@gmail.com>
2024-02-24 08:14:10 +0100

    added documentation for voting factory and voting contract

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,23 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+/// @title Voting contract
+/// @notice This contract allows users to vote on a pool of options
 contract Voting {
+    // user that created the voting pool
     address public owner;
 
+    // total number of votes cast
     uint256 public totalVotes;
-
+    
     struct Pool {
-        string description;
-        string[] options;
-        uint256[] votes;
+        string description; // The description of the pool users are voting on
+        string[] options; // The options users can vote on
+        uint256[] votes; // The number of votes each option has received
     }
 
     Pool _pool;
 
+    // mapping to check if a user has voted
     mapping(address => bool) public hasVoted;
 
+    /// @notice Event emitted when a user successfully votes
+    /// @param _voter the address of the user that voted
+    /// @param _option the option the user voted for
     event VoteSuccessful(address indexed _voter, uint256 _option);
 
+    /// @notice Constructor to create a new voting pool
+    /// @param _owner the address of the user that created the pool
+    /// @param _description the description of the pool
+    /// @param _options the options users can vote on
     constructor(address _owner, string memory _description, string[] memory _options) {
         owner = _owner;
 
@@ -29,14 +41,19 @@ contract Voting {
         }
     }
 
+    /// @notice Get the description of the pool
+    /// @return the description of the pool
     function getDescription() public view returns (string memory) {
         return _pool.description;
     }
 
+    /// @notice Get the options users can vote on
     function getOptions() public view returns (string[] memory) {
         return _pool.options;
     }
 
+    /// @notice Get the number of votes for a particular option in the pool
+    /// @param _option the option to get the number of votes for
     function getVotesByOption(uint256 _option) public view returns (uint256) {
         require(hasVoted[msg.sender], "Cannot check votes until you have voted");
         require(_pool.options.length >= _option, "Option does not exist");
@@ -44,6 +61,9 @@ contract Voting {
         return _pool.votes[_option-1];
     }
 
+    /// @notice Allows a user to vote on a particular option
+    /// @param _option the option to vote for
+    /// @dev A user can only vote once and cannot vote for an option that does not exist
     function vote(uint256 _option) public {
         require(!hasVoted[msg.sender], "Cannot vote twice");
         require(_pool.options.length >= _option, "Option does not exist");
@@ -55,6 +75,8 @@ contract Voting {
         emit VoteSuccessful(msg.sender, _option);
     }
 
+    /// @notice Get the total number of options in the pool
+    /// @return the total number of options
     function getTotalOptions() public view returns(uint256) {
         return _pool.options.length;
     }
--- contracts/VotingFactory.sol
+++ contracts/VotingFactory.sol
@@ -3,19 +3,30 @@ pragma solidity ^0.8.24;
 
 import "./Voting.sol";
 
-
+/// @title VotingFactory contract
+/// @notice Can be used to create new voting pools
 contract VotingFactory {
+    // deployer of the contract
     address public owner;
+
+    // voting pools created
     Voting[] votingPools;
 
+    // mapping of voting pool address to owner address
     mapping(address => address) poolOwner;
 
+    /// @notice Event emitted when a new voting pool is created
+    /// @param _poolAddress the address of the voting pool
+    /// @param _owner the address of the user that created the pool
     event VotingPoolCreated(address indexed _poolAddress, address indexed _owner);
 
     constructor() {
         owner = msg.sender;
     }
 
+    /// @notice Create a new voting pool
+    /// @param _description the description of the pool
+    /// @param _options the options users can vote on
     function createVotingPool(string memory _description, string[] memory _options) public {
         Voting _newPool = new Voting(msg.sender, _description, _options);
         votingPools.push(_newPool);
@@ -25,6 +36,8 @@ contract VotingFactory {
         emit VotingPoolCreated(_poolAddress, msg.sender);
     }
 
+    /// @notice Get the most recent voting pool created
+    /// @return the most recent voting pool
     function getMostRecentPool() public view returns (Voting) {
         uint256 _totalPools = votingPools.length;
         require(_totalPools > 0, "No voting pool has been created");
